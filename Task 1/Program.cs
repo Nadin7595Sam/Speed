f02697d4cde1b7db4935720d57583ef0367d2acf
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-09 13:55:44 +0300

    add task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int distance = 100;
+int friend = 2;
+int count = 0;
+
+Console.WriteLine();
+
+while (distance > 10)
+{
+    int time = 0;
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        Console.Write("time = ");
+        Console.WriteLine(time);
+    }
+    else
+        time = distance / (secondFriendSpeed + dogSpeed);
+        Console.Write("time = ");
+        Console.WriteLine(time);
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    Console.Write("distance = ");
+    Console.WriteLine(distance);
+    Console.WriteLine();
+    count++;
+}
+
+Console.Write("count = ");
+Console.WriteLine(count);
+Console.WriteLine();
