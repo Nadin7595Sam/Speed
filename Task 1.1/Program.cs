44aa409c4b141a0f83ec67e2752befc0daf3851d
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-10 13:42:35 +0300

    done task 1.1

---
+++ Task 1.1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 — это перевод из строки в число
+Console.WriteLine($"Результат: {n * n}"); // $"...:{}" — это интерполяция строк, чтобы не складывать строку с числом
