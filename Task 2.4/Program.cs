4ed063e9166ce8ff0c40b3f08375757783e4dd81
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 15:09:26 +0300

    done task 2.4

---
+++ Task 2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+// Первый вариант:
+/*int a = new Random().Next(1, 11);
+Console.WriteLine(a);
+int b = new Random().Next(1, 11);
+Console.WriteLine(b);
+int c = a % b;
+
+if (a % b == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine("Не кратно");
+    Console.WriteLine($"Остаток: {c}");*/
+
+// Второй вариант:
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Не кратно. Остаток: {a % b}");
