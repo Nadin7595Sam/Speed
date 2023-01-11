7ec356b7183c3380569a8256b3ed9447594b3858
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 15:58:00 +0300

    done task 2.2.1

---
+++ Task 2.2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100;
+int n2 = n / 10 % 10;
+int n3 = n % 10;
+
+if (n > 100 && n < 1000)
+    Console.WriteLine(n2);
+else
+    Console.WriteLine("Число не трёхзначное!");
