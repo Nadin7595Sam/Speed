f307a5be6ce39f71219d744a7b6527ea57eb01e9
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 15:32:12 +0300

    done task 2.6

---
+++ Task 2.6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b || b == a * a)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
