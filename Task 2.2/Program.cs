3975c068faa0e1d33d79a6f564271073501319aa
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 13:36:57 +0300

    done task 2.2

---
+++ Task 2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int n = new Random().Next(100, 1000); // [begin, end + 1] [100, 999]
+Console.WriteLine($"Случайное число: {n}");
+
+// Первый способ:
+/*int n1 = n / 100; //кол-во сотен
+int n2 = n / 10; // кол-во десятков
+int n3 = n % 10; // кол-во единиц
+
+Console.WriteLine($"{n1}{n3}");*/
+
+// Второй способ:
+int n1 = n / 100; //кол-во сотен
+int n3 = n % 10; // кол-во единиц
+
+Console.WriteLine(n1 * 10 + n3);
