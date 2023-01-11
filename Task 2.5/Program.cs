63022f00e2c0365268b6384a7d191c1a74432687
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 15:21:37 +0300

    done task 2.5

---
+++ Task 2.5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+// Первый вариант:
+/*if (a % 7 == 0 && a % 23 == 0)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");*/
+
+// Второй вариант:
+if (a % 7 == 0)
+{
+    if (a % 23 == 0)
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
+else
+    Console.WriteLine("Нет");
