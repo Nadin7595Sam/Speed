91acb265a528ff33c9107fdd9fd76d217ef96d73
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-11 13:59:23 +0300

    done task 2.3

---
+++ Task 2.3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+string? s = Console.ReadLine(); // ? — убираем желтое предупреждение
+Console.WriteLine(Convert.ToInt32(s[0].ToString()) * 5);
+// s[0] — символ строки
+// ToString() — перевод в строку
+// Convert.ToInt32 — перевод из строки в число по UTF-8
+
