ec02209dba6c4d8b71d32b73ce2daadc8712e433
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-12 22:30:34 +0300

    done task 3.1

---
+++ Task 3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// task 17
+
+Console.Clear();
+Console.Write("Введите координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+while (x == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+
+while (y == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
