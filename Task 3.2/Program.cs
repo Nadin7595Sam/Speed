590b92a84ca4d60381a4b2e7c9cba988b6145a81
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-12 22:37:00 +0300

    done task 3.2

---
+++ Task 3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// task 17
+
+Console.Clear();
+Console.Write("Введите четверть: ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+if (n == 1 && n == 2)
+    Console.WriteLine("y > 0");
+else
+    Console.WriteLine("y < 0");
+
+if (n == 1 && n == 4)
+    Console.WriteLine("x < 0");
+else
+    Console.WriteLine("x < 0");
