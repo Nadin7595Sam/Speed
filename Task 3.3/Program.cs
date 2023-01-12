ee8728cbce7c0c38ec23999a908423625c0caae1
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-12 23:18:32 +0300

    done task 3.3

---
+++ Task 3.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+/*Console.WriteLine(Math.Abs(-25)); // математически из любого числа делает модуль*/
+Console.Write("Введите x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine(Math.Round(dist, 2));
