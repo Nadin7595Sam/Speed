d82238d1bf7f543f7df638141a9c81c84778a4b5
Nadezhda <nadyusha-serdyuk@inbox.ru>
2023-01-13 15:35:33 +0300

    done task 3.4

---
+++ Task 3.4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    // 1-й вариант
+    //Console.Write($"{i * i} ");
+
+    // 2-й вариант
+    Console.Write($"{Math.Pow(i, 2)} ");
